7d67194dc462f48f3efeafbb10965473308ad334
Ee Durbin <ewdurbin@gmail.com>
2024-12-10 09:55:55 -0500

    pin

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     include_package_data = True, # include files listed in MANIFEST.in
     install_requires=[
         'Flask', 'MarkupSafe', 'decorator', 'itsdangerous', 'six', 'brotlipy',
-        'raven[flask]', 'werkzeug', 'gevent', 'flasgger'
+        'raven[flask]', 'werkzeug<3.0.0', 'gevent', 'flasgger'
     ],
 )
